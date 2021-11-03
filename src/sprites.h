c16d68aa35039d6d535a5a0a0576637328f3da8d
Wendel Scardua <wendelscardua@gmail.com>
2021-11-02 23:36:53 -0300

    Arrow cursor sprite

--- src/sprites.h
+++ src/sprites.h
@@ -1,1 +1,11 @@
 #include "constants.h"
+
+const unsigned char arrow_cursor_sprite[]={
+                                           0,  0,0x00,0,
+                                           8,  0,0x01,0,
+                                           0,  8,0x10,0,
+                                           8,  8,0x11,0,
+                                           0, 16,0x20,0,
+                                           8, 16,0x21,0,
+                                           0x80
+};
