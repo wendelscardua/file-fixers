43c261ac7e6ded500c6007283cd9bbd29fa39ea9
Wendel Scardua <wendelscardua@gmail.com>
2021-11-04 20:40:04 -0300

    Add dungeon palette

--- assets/palettes.h
+++ assets/palettes.h
@@ -1,2 +1,3 @@
 extern unsigned char bg_palette[];
+extern unsigned char dungeon_bg_palette[];
 extern unsigned char sprites_palette[];
--- assets/palettes.s
+++ assets/palettes.s
@@ -1,6 +1,7 @@
 .segment "RODATA"
 
-.export _bg_palette, _sprites_palette
+.export _bg_palette, _dungeon_bg_palette, _sprites_palette
 
 _bg_palette: .incbin "bg.pal"
+_dungeon_bg_palette: .incbin "bg-dungeon.pal"
 _sprites_palette: .incbin "sprites.pal"
