8a93df86b6d1612e854ba01e227a5d6e7a8596de
Wendel Scardua <wendelscardua@gmail.com>
2021-11-06 16:36:15 -0300

    Allow rle data to be unrle'd to buffer

--- src/lib/unrle.h
+++ src/lib/unrle.h
@@ -1,1 +1,3 @@
 void __fastcall__ unrle(unsigned char * rle_data);
+void __fastcall__ set_unrle_buffer(unsigned char * rle_buffer);
+void __fastcall__ unrle_to_buffer(unsigned char * rle_data);
--- src/lib/unrle.s
+++ src/lib/unrle.s
@@ -7,6 +7,7 @@ PPUDATA = $2007
 rle_ptr: .res 2
 rle_tag: .res 1
 rle_byte: .res 1
+rle_buffer_ptr: .res 2
 
 .segment "CODE"
 
@@ -47,5 +48,50 @@ the_end:
   BNE :+
   INC rle_ptr+1
 :
+  RTS
+.endproc
+
+.export _set_unrle_buffer
+.proc _set_unrle_buffer
+  STA rle_buffer_ptr
+  STX rle_buffer_ptr+1
+  RTS
+.endproc
+
+.export _unrle_to_buffer
+.proc _unrle_to_buffer
+  STA rle_ptr
+  STX rle_ptr+1
+  ; Decompress data from address on rle_ptr to PPUDATA
+  LDY #0
+  JSR fetch_rle_byte
+  STA rle_tag
+fetch_command:
+  JSR fetch_rle_byte
+  CMP rle_tag
+  BEQ tag_found
+  STA (rle_buffer_ptr), Y
+  INC rle_buffer_ptr
+  BNE :+
+  INC rle_buffer_ptr+1
+:
+  STA rle_byte
+  BNE fetch_command
+tag_found:
+  JSR fetch_rle_byte
+  CMP #0
+  BEQ the_end
+  TAX
+  LDA rle_byte
+write_rle:
+  STA (rle_buffer_ptr), Y
+  INC rle_buffer_ptr
+  BNE :+
+  INC rle_buffer_ptr+1
+:
+  DEX
+  BNE write_rle
+  JMP fetch_command
+the_end:
   RTS
 .endproc
