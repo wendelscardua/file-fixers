a51952ff2a8765182ef7ebb5aed77afff64fae11
Wendel Scardua <wendelscardua@gmail.com>
2021-11-07 16:47:40 -0300

    Some Tiled dungeon layouts

---
+++ assets/dungeons/metatiles.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="metatiles" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="metatiles.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
