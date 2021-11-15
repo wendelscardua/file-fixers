34aec8aa2810d28a08550a989d16dc8f527847ab
Wendel Scardua <wendelscardua@gmail.com>
2021-11-15 15:15:51 -0300

    Fix player names

--- src/wram.c
+++ src/wram.c
@@ -6,7 +6,7 @@
 #pragma bss-name(push, "XRAM")
 // extra RAM at $6000-$7fff
 
-#define WRAM_VERSION 0x000c
+#define WRAM_VERSION 0x000d
 unsigned int wram_start;
 
 unsigned char dungeon_layout_initialized;
--- src/wram.h
+++ src/wram.h
@@ -11,7 +11,7 @@ extern unsigned char wram_dungeon_layout[];
 extern unsigned char party_level;
 extern unsigned char player_xp[];
 extern player_class_type player_class[];
-extern unsigned char * player_name[];
+extern unsigned char player_name[4][5];
 
 extern entity_type_enum entity_type[];
 extern unsigned char entity_lv[];
